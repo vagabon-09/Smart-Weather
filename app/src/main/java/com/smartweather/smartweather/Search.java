30063012276c0ec2b281c3340471d99b62bb7cc0
Rajesh <rajeshbhadra62@gmail.com>
2022-08-18 03:00:07 +0530

    Click on first card , Show the popup window.

--- app/src/main/java/com/smartweather/smartweather/Search.java
+++ app/src/main/java/com/smartweather/smartweather/Search.java
@@ -5,14 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.KeyEvent;
+import android.view.View;
 import android.view.WindowManager;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.zip.Inflater;
 
 public class Search extends AppCompatActivity {
 EditText search_field;
+ImageView micBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +27,17 @@ EditText search_field;
         getId();
         //Action on click keyboard search
         lisentEdit();
+        //Add one click 
+        micBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                clicMic();
+            }
+        });
+    }
+
+    private void clicMic() {
+        Toast.makeText(this, "Yes mic is working....", Toast.LENGTH_SHORT).show();
     }
 
     //Sending search value to MainActivity and listening search from keyboard
@@ -49,5 +64,6 @@ EditText search_field;
     //Finding view through id
     private void getId() {
         search_field = findViewById(R.id.edit_search_field_id);
+        micBtn = findViewById(R.id.mic_icon_btn);
     }
 }
