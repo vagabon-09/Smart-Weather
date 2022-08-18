43cf9c5110e9ae1e5e8d8ba3b91d6a140091c4f7
Rajesh <rajeshbhadra62@gmail.com>
2022-08-18 15:43:38 +0530

    Voice search Features is add

--- app/src/main/java/com/smartweather/smartweather/Search.java
+++ app/src/main/java/com/smartweather/smartweather/Search.java
@@ -1,9 +1,13 @@
 package com.smartweather.smartweather;
 
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.WindowManager;
@@ -12,11 +16,15 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.Locale;
+import java.util.Objects;
 import java.util.zip.Inflater;
 
 public class Search extends AppCompatActivity {
 EditText search_field;
 ImageView micBtn;
+private final int REQUEST_CODE_EXTRA_INPUT =1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,7 +45,27 @@ ImageView micBtn;
     }
 
     private void clicMic() {
-        Toast.makeText(this, "Yes mic is working....", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,REQUEST_CODE_EXTRA_INPUT);
+        try {
+            startActivityForResult(intent,REQUEST_CODE_EXTRA_INPUT);
+        }catch (Exception e){
+            Log.d("Error Voice", "Mic Error:  "+e);
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE_EXTRA_INPUT){
+            if(resultCode == RESULT_OK && data!=null){
+                ArrayList<String> arrayList = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                search_field.setText(Objects.requireNonNull(arrayList).get(0));
+            }
+        }
     }
 
     //Sending search value to MainActivity and listening search from keyboard
